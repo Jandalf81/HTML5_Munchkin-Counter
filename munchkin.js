b1ade252691897e6d5ad18d01a45c1099589a3a5
Jandalf81 <janschmitte@gmail.com>
2026-01-30 14:34:27 +0100

    Second version

--- munchkin.js
+++ munchkin.js
@@ -1,1 +1,54 @@
-function decrement(id) {    value = document.getElementById(id).innerText;    if (value > 0) {        value--        document.getElementById(id).innerText = value;        id = id + "-invert"        document.getElementById(id).innerText = value;        updateStrength();    }}function increment(id) {    value = document.getElementById(id).innerText;    value++    document.getElementById(id).innerText = value;    id = id + "-invert"    document.getElementById(id).innerText = value;    updateStrength();}function updateStrength() {    level = Number(document.getElementById('level').innerText);    bonus = Number(document.getElementById('bonus').innerText);    strength = level + bonus    document.getElementById('strength').innerText = strength;    document.getElementById('strength-invert').innerText = strength;    saveValues(level, bonus);}function saveValues(level, bonus) {    localStorage.setItem('level', level);    localStorage.setItem('bonus', bonus);    console.log('Level: ' + level);}function onLoad() {    console.log('Getting values from localStorage...');    level = Number(localStorage.getItem('level'));    bonus = Number(localStorage.getItem('bonus'));    document.getElementById('level').innerText = level;    document.getElementById('level-invert').innerText = level;    document.getElementById('bonus').innerText = bonus;    document.getElementById('bonus-invert').innerText = bonus;    strength = level + bonus;    document.getElementById('strength').innerText = strength;    document.getElementById('strength-invert').innerText = strength;}onLoad();
+function decrement(id) {
+    value = Number(document.getElementById(id).innerText);
+    if (id == 'level' && value > 1 || id == 'bonus' && value > 0) {
+        value--
+        document.getElementById(id).innerText = value;
+        document.getElementById(id + '-invert').innerText = value;
+
+        updateStrength();
+    }
+}
+
+function increment(id) {
+    value = Number(document.getElementById(id).innerText);
+    value++
+    document.getElementById(id).innerText = value;
+    document.getElementById(id + '-invert').innerText = value;
+
+    updateStrength();
+}
+
+function updateStrength() {
+    level = Number(document.getElementById('level').innerText);
+    bonus = Number(document.getElementById('bonus').innerText);
+    strength = level + bonus
+
+    document.getElementById('strength').innerText = strength;
+    document.getElementById('strength-invert').innerText = strength;
+
+    saveValues(level, bonus);
+}
+
+function saveValues(level, bonus) {
+    localStorage.setItem('level', level);
+    localStorage.setItem('bonus', bonus);
+
+    console.log('Level: ' + level);
+}
+
+function onLoad() {
+    console.log('Getting values from localStorage...');
+    level = Number(localStorage.getItem('level'));
+    bonus = Number(localStorage.getItem('bonus'));
+
+    document.getElementById('level').innerText = level;
+    document.getElementById('level-invert').innerText = level;
+    document.getElementById('bonus').innerText = bonus;
+    document.getElementById('bonus-invert').innerText = bonus;
+
+    strength = level + bonus;
+    document.getElementById('strength').innerText = strength;
+    document.getElementById('strength-invert').innerText = strength;
+}
+
+onLoad();
